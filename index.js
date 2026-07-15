5c6f47fa95973c52c4c128f02189e08b62cbae66
salmanalfarizi3 <salmanalfariziiel@gmail.com>
2026-07-15 14:43:48 +0700

    implement GET /biodata route

--- index.js
+++ index.js
@@ -14,3 +14,16 @@ const pool = new Pool({
 
 app.use(express.json());
 
+app.get('/biodata', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM biodata ORDER BY id ASC');
+        res.status(200).json({
+            message: "Berhasil mengambil data biodata",
+            data: result.rows
+        });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).json({ error: "Terjadi kesalahan pada server atau database" });
+    }
+});
+
