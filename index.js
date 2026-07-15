fcdd37de7a9970045279687e481cb561b9541fe0
salmanalfarizi3 <salmanalfariziiel@gmail.com>
2026-07-15 14:37:04 +0700

    setup express server and postgres connection

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const { Pool } = require('pg');
+
+const app = express();
+const port = 3000;
+
+const pool = new Pool({
+    user: 'postgres',          
+    host: 'localhost',          
+    database: 'mahasiswa',    
+    password: '2025',  
+    port: 5432,                 
+});
+
